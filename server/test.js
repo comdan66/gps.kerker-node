669ee9129d5db50dd45fd0ca3d7b58624b9f454a
OA Wu <comdan66@gmail.com>
2020-08-27 23:47:17 +0800

    Fix code.

---
+++ server/test.js
@@ -0,0 +1,18 @@
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2020, Ginkgo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+require('./sys/core/App')({
+  data: {
+    title: '測試',
+  },
+  methods: {
+    
+  },
+  init () {
+    
+  }
+})
